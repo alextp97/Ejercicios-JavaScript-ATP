47a5aa4287752727e6552ac82b3c04d7493730f0
Alejandro Torres <alejandro.torres@bosonit.com>
2022-06-15 10:11:38 +0200

    Acabados los dos primeros ejercicios

---
+++ Ejercicio-1/Ejercicio_1.js
@@ -0,0 +1,12 @@
+//Dado un array de objetos, obtener el objeto con el id 3
+
+const arrNames = [
+    {id: 1, name: 'Pepe'},
+    {id: 2, name: 'Juan'},
+    {id: 3, name: 'Alba'},
+    {id: 4, name: 'Toby'},
+    {id: 5, name: 'Lala'}
+  ];
+
+const item = arrNames.find( (elemento) => elemento.id === 3);
+  console.log(item)
